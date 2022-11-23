1e1398192d4e528b46bc50dd64c1c654dfb7dcf8
Oguzkan-school <56897627+OguzkanK@users.noreply.github.com>
2022-11-23 12:58:04 +0300

    Google Maps API entegrasyonu

--- joggernaut/ios/Runner/AppDelegate.swift
+++ joggernaut/ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyD9JG8aJpFUx519Ww2qc0K5fazfeW7QyZY")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
