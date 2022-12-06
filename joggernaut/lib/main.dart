911bbb284d7a0ae5f06c5f99309b91ad1e4613a3
Kaanium <kaan.koroglu@ogr.iuc.edu.tr>
2022-12-06 12:30:49 +0300

    Arka planda çalışma

--- joggernaut/lib/main.dart
+++ joggernaut/lib/main.dart
@@ -1,115 +1,227 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+
+import 'package:pedometer/pedometer.dart';
+import 'package:percent_indicator/percent_indicator.dart';
+import 'package:intl/intl.dart';
+import 'package:flutter_foreground_service/flutter_foreground_service.dart';
+
+DateTime now = DateTime.now();
+
+String formatDate(DateTime d) {
+  return d.toString().substring(0, 19);
+}
 
 void main() {
-  runApp(const MyApp());
+  runApp(MaterialApp(home: MyApp()));
+  startForegroundService();
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+void startForegroundService() async {
+  ForegroundService().start();
+}
 
-  // This widget is the root of your application.
+class MyApp extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int previousSteps = 0;
+  late Stream<StepCount> _stepCountStream;
+  late Stream<PedestrianStatus> _pedestrianStatusStream;
+  String _status = '?', _steps = '0';
+
+  @override
+  void initState() {
+    super.initState();
+    Timer.periodic(Duration(seconds: 15), (Timer t) => dayChecker());
+    initPlatformState();
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  double percentageCal() {
+    double percentage;
+    try {
+      percentage = double.parse(dailySteps()) / 10000.0;
+    } catch (e) {
+      percentage = 0.0;
+    }
+    return percentage;
+  }
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
+  void onStepCount(StepCount event) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _steps = event.steps.toString();
     });
   }
 
+  void onPedestrianStatusChanged(PedestrianStatus event) {
+    setState(() {
+      _status = event.status;
+    });
+  }
+
+  void onPedestrianStatusError(error) {
+    setState(() {
+      _status = 'Pedestrian Status not available';
+    });
+  }
+
+  void onStepCountError(error) {
+    setState(() {
+      _steps = '0';
+    });
+  }
+
+  void initPlatformState() {
+    _pedestrianStatusStream = Pedometer.pedestrianStatusStream;
+    _pedestrianStatusStream
+        .listen(onPedestrianStatusChanged)
+        .onError(onPedestrianStatusError);
+
+    _stepCountStream = Pedometer.stepCountStream;
+    _stepCountStream.listen(onStepCount).onError(onStepCountError);
+
+    if (!mounted) return;
+  }
+
+  void dayChecker() {
+    if (DateFormat.Hm().format(DateTime.now()) == "00:00") {
+      previousSteps = int.parse(_steps);
+    }
+  }
+
+  String dailySteps() {
+    return (int.parse(_steps) - previousSteps).toString();
+  }
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Joggernaut'),
+          backgroundColor: const Color.fromARGB(255, 124, 77, 255),
+        ),
+        body: Container(
+          decoration: const BoxDecoration(
+              gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Color.fromARGB(255, 240, 255, 66),
+              Color.fromARGB(255, 55, 146, 55)
+            ],
+          )),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Image.asset('Assets/logopng.png',
+                        width: 130, height: 130, alignment: Alignment.topLeft),
+                    CircularPercentIndicator(
+                      radius: 80.0,
+                      lineWidth: 15.0,
+                      percent: percentageCal(),
+                      center: Text(
+                        dailySteps(),
+                        style: const TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 15.0),
+                      ),
+                      backgroundColor: const Color.fromARGB(255, 130, 205, 71),
+                      circularStrokeCap: CircularStrokeCap.butt,
+                      progressColor: const Color.fromARGB(255, 124, 77, 255),
+                    )
+                  ]),
+              const Divider(
+                height: 100,
+                thickness: 0,
+                color: Color.fromARGB(255, 124, 77, 255),
+              ),
+              const Text(
+                'Pedestrian status:',
+                style: TextStyle(fontSize: 15),
+              ),
+              Icon(
+                _status == 'walking'
+                    ? Icons.directions_walk
+                    : _status == 'stopped'
+                        ? Icons.accessibility_new
+                        : Icons.error,
+                size: 100,
+              ),
+              Center(
+                child: Text(
+                  _status,
+                  style: _status == 'walking' || _status == 'stopped'
+                      ? const TextStyle(fontSize: 30)
+                      : const TextStyle(fontSize: 20),
+                ),
+              ),
+              Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    SizedBox(
+                        width: (MediaQuery.of(context).size.width /
+                            4), // <-- Your width
+                        height: 50,
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          child: const Text('Home'),
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor:
+                                  const Color.fromARGB(255, 124, 77, 255),
+                              textStyle: const TextStyle(
+                                  fontSize: 12.0, fontWeight: FontWeight.bold)),
+                        )),
+                    SizedBox(
+                        width: (MediaQuery.of(context).size.width /
+                            4), // <-- Your width
+                        height: 50,
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          child: const Text('Map'),
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor:
+                                  const Color.fromARGB(255, 124, 77, 255),
+                              textStyle: const TextStyle(
+                                  fontSize: 12.0, fontWeight: FontWeight.bold)),
+                        )),
+                    SizedBox(
+                        width: (MediaQuery.of(context).size.width /
+                            4), // <-- Your width
+                        height: 50,
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          child: const Text('Race'),
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor:
+                                  const Color.fromARGB(255, 124, 77, 255),
+                              textStyle: const TextStyle(
+                                  fontSize: 12.0, fontWeight: FontWeight.bold)),
+                        )),
+                    SizedBox(
+                        width: (MediaQuery.of(context).size.width /
+                            4), // <-- Your width
+                        height: 50,
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          child: const Text('Leaderboard'),
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor:
+                                  const Color.fromARGB(255, 124, 77, 255),
+                              textStyle: const TextStyle(
+                                  fontSize: 12.0, fontWeight: FontWeight.bold)),
+                        )),
+                  ])
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
