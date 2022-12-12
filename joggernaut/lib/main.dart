5361ded8197b0a5ab61ffa0396409b9f33382bf2
Kaanium <kaan.koroglu@ogr.iuc.edu.tr>
2022-12-12 20:02:02 +0300

    merge

--- joggernaut/lib/main.dart
+++ joggernaut/lib/main.dart
@@ -1,8 +1,38 @@
+// import 'package:flutter/material.dart';
+// import 'steps.dart';
+
+// void main() {
+//   runApp(
+//       const MaterialApp(home: StepsPage(), debugShowCheckedModeBanner: false));
+//   startForegroundService();
+// }
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
+import 'ZEYNEP/GoogleSign.dart';
 import 'steps.dart';
 
-void main() {
-  runApp(
-      const MaterialApp(home: StepsPage(), debugShowCheckedModeBanner: false));
+import 'ZEYNEP/RedirectPage.dart'; // Firebase import
+
+// Firebase import
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized(); //Firebase starting
+  await Firebase.initializeApp(); //Firebase starting
+  runApp(const MyApp());
   startForegroundService();
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => ChangeNotifierProvider(
+        create: (context) => GoogleSignInProvider(),
+        child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          home: MainPage(),
+        ),
+      );
 }
